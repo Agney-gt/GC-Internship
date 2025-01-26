a301e77623adc0a4f3a952c1aabb37d1e4adce57
Kajalmishra617 <kjmishra03@gmail.com>
2025-01-26 15:43:45 +0530

    Create kajal.py

---
+++ kajal.py
@@ -0,0 +1,15 @@
+Selenium Google Scraper
+Selenium Google Scraper is a Python script for extracting Google search result titles, links, and snippets.
+
+Installation
+Install Python from Python
+
+pip install selenium
+Download Webdriver for the browser: For chrome: Here
+
+Usage
+Update the search_query variable in the script with your desired search term.
+
+Run the script in your terminal
+
+python scraper.py
